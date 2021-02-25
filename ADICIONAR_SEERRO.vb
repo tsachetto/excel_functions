7393d387aa35d4ebac045e76f654af6832a5e75c
thomaz sachetto <thomazsachetto@gmail.com>
2021-02-25 13:12:02 -0300

    Create ADICIONAR_SEERRO.vb

---
+++ ADICIONAR_SEERRO.vb
@@ -0,0 +1,19 @@
+Sub addseerrosel()
+  
+    'Função destinada a adicionar a fórmula "=SEERRO" em todas as células da Active Selection.
+  
+  Dim se_erro As String
+  
+  se_erro = """"""    
+    
+    For Each cell In Range(Selection, Selection)
+  
+      form = cell.FormulaLocal
+      If Len(form) > 0 Then
+        formul = "=SEERRO(" & Mid(form, 2, Len(form)) & ";" & se_erro & ")"
+        cell.FormulaLocal = formul
+      Else: End If
+    
+    Next
+    
+End Sub
