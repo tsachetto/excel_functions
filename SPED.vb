65ca9222b6e31e6ceac7a73b1f4413654807597e
thomaz sachetto <thomazsachetto@gmail.com>
2021-09-25 03:14:27 -0300

    Update SPED.vb

--- SPED.vb
+++ SPED.vb
@@ -1,9 +1,9 @@
-Public Function SPED3(sped As String, amtI As Integer)
+Public Function SPED(sped As String, amtI As Integer)
 Dim ini As Integer, fim As Integer, count As Integer
 ini = 0
 For i = 1 To amtI
 ini = InStr((ini + 1), sped, "|")
 fim = InStr((ini + 1), sped, "|")
 Next
-SPED3 = Mid(sped, (ini + 1), (fim - ini - 1))
+SPED = Mid(sped, (ini + 1), (fim - ini - 1))
 End Function
