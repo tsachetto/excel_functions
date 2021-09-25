365625acb206e6bc5813e3e5778b592ace3b74d2
thomaz sachetto <thomazsachetto@gmail.com>
2021-09-25 03:14:12 -0300

    Update SPED.vb

--- SPED.vb
+++ SPED.vb
@@ -1,43 +1,9 @@
-Public Function SPED(cel As String, qtd As Integer)
-
-'Como utilizar: a função extrai valores de qualquer coluna de uma referida linha do SPED, sem a necessidade de quebrar o arquivo em colunas.
-' =SPED(Célula contendo o bloco de layout, Número Coluna que vc quer)
-'Exemplo: =SPED(A1;5) irá retornar a quinta coluna do layout colado na célula A1.
-
-Dim posicao As Integer
-Dim incial As Integer
-Dim final As Integer
-Dim tamanho As Integer
-Dim linhasped As String
-Dim separador As String
-
-separador = "|"
-linhasped = cel
-tamanho = Len(linhasped)
-
-For i = 1 To tamanho
-
-    carac = Mid(linhasped, i, 1)
-        
-    If carac = separador Then
-    
-        posicao = posicao + 1
-    
-        If posicao = qtd Then
-            incial = i
-        Else: End If
-        
-        If posicao = qtd + 1 Then
-            final = i
-            GoTo concluir
-        Else: End If
-    
-    Else: End If
-        
-Next i
-    
-concluir:
-
-    SPED = Mid(cel, (incial + 1), (final - incial - 1))
-
+Public Function SPED3(sped As String, amtI As Integer)
+Dim ini As Integer, fim As Integer, count As Integer
+ini = 0
+For i = 1 To amtI
+ini = InStr((ini + 1), sped, "|")
+fim = InStr((ini + 1), sped, "|")
+Next
+SPED3 = Mid(sped, (ini + 1), (fim - ini - 1))
 End Function
