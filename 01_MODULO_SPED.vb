00a2957e97d2b2d8ea90ec0a15f83f1dd70d0f7f
thomaz sachetto <thomazsachetto@gmail.com>
2021-09-25 03:27:37 -0300

    Update 01_MODULO_SPED.vb

--- 01_MODULO_SPED.vb
+++ 01_MODULO_SPED.vb
@@ -2,95 +2,30 @@
 'Basta copiar tudo e colar em um novo módulo VBA no MS Excel
 
 
-Public Function SPED(cel As String, qtd As Integer)
-
-'Como utilizar: a função extrai valores de qualquer coluna de uma referida linha do SPED, sem a necessidade de quebrar o arquivo em colunas.
-' =SPED(Célula contendo o bloco de layout, Número Coluna que vc quer)
-'Exemplo: =SPED(A1;5) irá retornar a quinta coluna do layout colado na célula A1.
-
-Dim posicao As Integer
-Dim incial As Integer
-Dim final As Integer
-Dim tamanho As Integer
-Dim linhasped As String
-Dim separador As String
-
-separador = "|"
-linhasped = cel
-tamanho = Len(linhasped)
-
-For i = 1 To tamanho
-
-    carac = Mid(linhasped, i, 1)
-        
-    If carac = separador Then
-    
-        posicao = posicao + 1
-    
-        If posicao = qtd Then
-            incial = i
-        Else: End If
-        
-        If posicao = qtd + 1 Then
-            final = i
-            GoTo concluir
-        Else: End If
-    
-    Else: End If
-        
-Next i
-    
-concluir:
-
-    SPED = Mid(cel, (incial + 1), (final - incial - 1))
-
+Public Function SPED(sped As String, amtI As Integer)
+Dim ini As Integer, fim As Integer
+For i = 1 To amtI
+ini = InStr((ini + 1), sped, "|")
+fim = InStr((ini + 1), sped, "|")
+Next
+SPED = Mid(sped, (ini + 1), (fim - ini - 1))
 End Function
 
-Public Function ALTSPED(cel As String, nr As Long, vr) As String
+'---------
 
-'Como utilizar: a função ALTERA valores de qualquer coluna de uma referida linha do SPED, sem a necessidade de quebrar o arquivo em colunas.
-'=ALTSPED(Célula contendo o bloco de layout, Número Coluna que vc quer, Valor a ser Alterado)
-'Exemplo: =SPED(A1;6;"alt") irá retornar a linha com a sexta coluna do layout colado na célula A1 alterado para "alt".
-'|C181|06|5405|218,88|0|218,88|0|||0|2|   <vira>   |C181|06|5405|218,88|0|alt|0|||0|2|
-
-Dim posicao as Integer
-Dim inicial as Integer
-Dim final as Integer
-Dim tamanho As Long
-Dim tx as String
-Dim cr As String
-
-cr = "|"
-tx = cel
-tamanho = Len(tx)
-
-    For i = 1 To tamanho
-        
-        y = Mid(tx, i, 1)
-            
-            If y = cr Then
-            
-                posicao = posicao + 1
-            
-                    If posicao = nr Then
-                        inicial = i
-                    Else: End If
-                    
-                    If posicao = nr + 1 Then
-                        final = i
-                        GoTo xx
-                    Else: End If
-                    
-            Else: End If
-            
-    Next i
-    
-xx:
-p1 = Mid(cel, 1, inicial)
-p2 = Mid(cel, final, 150)
-ALTSPED = p1 & vr & p2
-
+Public Function ALTSPED(sped As String, amtI As Integer, ByVal change As String)
+'Utilize: =altsped("texto linha do sped |a|b|"; nr campo a ser alterado: 2, novo valor (ex. "x" ou 2)
+Dim ini As Integer, fim As Integer, pt1 As String, pt2 As String
+For i = 1 To amtI
+ini = InStr((ini + 1), sped, "|")
+fim = InStr((ini + 1), sped, "|")
+Next
+pt1 = Mid(sped, 1, ini)
+pt2 = Mid(sped, fim, Len(sped))
+ALTSPED = pt1 & change & pt2
 End Function
+
+'---------
 
 Public Function CONCATE(ByRef Separador As String, ByRef Area As Range, ByRef Ref As Integer) As String
 
